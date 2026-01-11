c27ca0ddddc938be5357f9c8ec2bb7c53fefcbda
Lead0x194 <not_found@lead.com>
2026-01-11 16:56:05 +0100

    fix(server_source): add __iob_func to shutup the compiler

--- Lead-Server-Source/db/src/version.cpp
+++ Lead-Server-Source/db/src/version.cpp
@@ -20,3 +20,12 @@ void WriteVersion()
 #endif
 }
 
+// TODO: Remove workaround by using mysqlclient build from vcpkg
+//       This is just needed to make the compiler shut up for now.
+extern "C" {
+	FILE* __cdecl __iob_func(void) {
+		static FILE* iob[3] = { stdin, stdout, stderr };
+		return iob[0];
+	}
+}
+
