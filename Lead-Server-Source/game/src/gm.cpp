3470dd45d2a62127bedfa803e8bd9ca8a04ff898
Lead0x194 <not_found@lead.com>
2026-01-17 17:42:22 +0100

    fix: identation in gm_new_get_level method

--- Lead-Server-Source/game/src/gm.cpp
+++ Lead-Server-Source/game/src/gm.cpp
@@ -85,22 +85,22 @@ BYTE gm_new_get_level( const char * name, const char * host, const char* account
 
 	    if ( host )
 	    {
-		if ( it->second.pset_Host )
-		{
-		    if ( it->second.pset_Host->end() == it->second.pset_Host->find( host ) )
-		    {
-			sys_log(0, "GM_NEW_GET_LEVEL : BAD HOST IN HOST_LIST");
-			return GM_PLAYER;
-		    }
-		}
-		else
-		{
-		    if ( strcmp ( it->second.Info.m_szContactIP, host  ) != 0 )
-		    {
-			sys_log(0, "GM_NEW_GET_LEVEL : BAD HOST IN GMLIST");
-			return GM_PLAYER;
-		    }
-		}
+			if ( it->second.pset_Host )
+			{
+			    if ( it->second.pset_Host->end() == it->second.pset_Host->find( host ) )
+			    {
+					sys_log(0, "GM_NEW_GET_LEVEL : BAD HOST IN HOST_LIST");
+					return GM_PLAYER;
+			    }
+			}
+			else
+			{
+			    if ( strcmp ( it->second.Info.m_szContactIP, host  ) != 0 )
+			    {
+					sys_log(0, "GM_NEW_GET_LEVEL : BAD HOST IN GMLIST");
+					return GM_PLAYER;
+			    }
+			}
 	    }
 	    sys_log(0, "GM_NEW_GET_LEVEL : FIND HOST");
 	    
